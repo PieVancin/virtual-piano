aeda982f2263597afc8f612da5779be3530275d8
Pierre <pie.vancin@gmail.com>
2021-03-15 18:37:39 -0300

    feat: add functional black keys

--- src/script.js
+++ src/script.js
@@ -20,7 +20,22 @@ document.addEventListener("keydown", function(event){
     } else if (event.code === 'KeyJ') {
         let j = new Audio("audio/J.mp3");
         j.play();
-    } else {
+    } else if (event.code === 'KeyW') {
+        let w = new Audio("audio/W.mp3");
+        w.play();
+    } else if (event.code === 'KeyE') {
+        let e = new Audio("audio/E.mp3");
+        e.play();
+    } else if (event.code === 'KeyT') {
+        let t = new Audio("audio/T.mp3");
+        t.play();
+    } else if (event.code === 'KeyY') {
+        let y = new Audio("audio/Y.mp3");
+        y.play();
+    } else if (event.code === 'KeyU') {
+        let u = new Audio("audio/U.mp3");
+        u.play();
+    }  else {
         console.log(`Invalid key.`);
     }
 })
